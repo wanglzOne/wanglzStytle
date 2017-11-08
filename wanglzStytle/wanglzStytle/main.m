617d7c22698792fa61dbf9eccc85cdca7f95a9c6
bhqm <bhqm@bhqmdeMacBook-Pro.local>
2017-11-08 18:06:53 +0800

    init commit

---
+++ wanglzStytle/wanglzStytle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wanglzStytle
+//
+//  Created by bhqm on 2017/11/8.
+//  Copyright © 2017年 wanglz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
